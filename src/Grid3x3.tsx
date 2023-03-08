1f50f7056b9479df6bcc24f96d5d9fb007966d4d
sakana7392 <k_hiro@kzd.biglobe.ne.jp>
2023-03-09 00:32:04 +0900

    決まっていない場所は空白に指定

--- src/Grid3x3.tsx
+++ src/Grid3x3.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 const rowStyle={
     display:"flex",
     margin:"0px",
@@ -28,24 +30,45 @@ const massFixedCss:React.CSSProperties={
     borderColor: 'red',
     borderStyle: 'dashed',
 }
+export const row3=(num:number)=>{
+    return(
+        <div className="item" style={massFixedCss}>
+            {num>0?num:" "}
+        </div>
+    );
+};
 export const Grid3x3=()=>{
+    const [gridNums,setGridNums]=useState<number[]>([...Array<number>(9)].map(()=>0));//  初期値は全て-1
+    useEffect(()=>{
+        
+        const initialNums:number[]=[...Array<number>(9)].map(()=>-1);
+        
+        
+    },[]);
+    const row3=(num:number)=>{
+        return(
+            <div className="item" style={massFixedCss}>
+                {num>0?num:" "}
+            </div>
+        );
+    };
     return(
         <div style={{ borderWidth:"0.3px",
         borderStyle:"solid",}}>
             <div className="container" style={rowStyle}>
-                <div className="item" style={massFixedCss}>1</div>
-                <div className="item" style={massFixedCss}>2</div>
-                <div className="item" style={massFixedCss}>3</div>
+                {row3(gridNums[0])}
+                {row3(gridNums[1])}
+                {row3(gridNums[2])}
             </div>
             <div className="container" style={rowStyle}>
-                <div className="item" style={massFixedCss}>1</div>
-                <div className="item" style={massFixedCss}>2</div>
-                <div className="item" style={massFixedCss}>3</div>
+                {row3(gridNums[3])}
+                {row3(gridNums[4])}
+                {row3(gridNums[5])}
             </div>
             <div className="container" style={rowStyle}>
-                <div className="item" style={massFixedCss}>1</div>
-                <div className="item" style={massFixedCss}>2</div>
-                <div className="item" style={massFixedCss}>3</div>
+                {row3(gridNums[6])}
+                {row3(gridNums[7])}
+                {row3(gridNums[8])}
             </div>
         </div>
     );
